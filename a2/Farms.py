120ccc7f7188feb4fa53eb49dffa77a4bad88e72
Sophie Ivanovic <sophieivanovic@Sophies-Air.gateway>
2024-03-31 11:08:56 +1000

    task sheet and provided code

---
+++ a2/Farms.py
@@ -0,0 +1,29 @@
+Farms = ['Cowbell', 'Creamy Acres', 'Milky Way', 'Happy Cows', 'Udder Delight', 'Fresh Pail', 'Cowabunga', 'Utopia', 'Moo Meadows', 'Bluebell', 'Harmony', 'Velvet Valley', 'Moonybrook', 'Cloven Hills', 'Midnight Moo', 'Willows Bend', 'Moosa Heads', 'Dreamy Dairies', 'Happy Hooves', 'Highlands']
+
+Supply = [5200, 9900, 8800, 6900, 9500, 5900, 3700, 4800, 3200, 3400, 4400, 4500, 3200, 4800, 3100, 3400, 3000, 4500, 4500, 3900]
+
+Distance = [
+	[94,102,76],
+	[70,91,84],
+	[93,83,58],
+	[103,88,61],
+	[59,79,86],
+	[107,72,44],
+	[74,54,62],
+	[94,57,44],
+	[44,62,93],
+	[34,71,103],
+	[51,51,83],
+	[87,42,46],
+	[114,47,21],
+	[25,70,107],
+	[56,39,79],
+	[66,30,70],
+	[42,54,94],
+	[44,56,98],
+	[96,29,66],
+	[87,44,82]
+]
+
+PMin = [20000,24000,32000]
+PMax = [45000,35000,36000]
